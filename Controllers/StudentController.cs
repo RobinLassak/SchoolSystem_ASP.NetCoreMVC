58763f2247236994cc05849a974550bf7cdad001
Robin <lassak.robin@gmail.com>
2025-05-30 13:54:49 +0200

    Student controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCoreMVC_SchoolSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
