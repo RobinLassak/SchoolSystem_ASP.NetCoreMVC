cd975a617b288fbc177655fdf7f0497af3741a57
Robin <lassak.robin@gmail.com>
2025-05-30 13:35:06 +0200

    StudentDTO

---
+++ DTO/StudentDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NetCoreMVC_SchoolSystem.DTO
+{
+    public class StudentDTO
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
