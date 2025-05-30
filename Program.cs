bb6c45bfc7367ca3f26c49eec8aba5cb2cd596a5
Robin <lassak.robin@gmail.com>
2025-05-30 10:59:45 +0200

    Initial commit of ASP.NET Core MVC project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+//Dependency Injection
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. - Zalezi na poradi prikazu tzv. middleware
+
+//Tato cast rika ze middleware je omezen jen na produkni prostredi
+if (!app.Environment.IsDevelopment())
+{
+    //Smeruje chybove hlasky na urcitou cestu
+    app.UseExceptionHandler("/Home/Error");
+
+    //Aktivuje bezpecnostni mechanismus ktery chrani web
+    app.UseHsts();
+}
+
+//Vynucuje pouziti HTTPS
+app.UseHttpsRedirection();
+
+//Pro pouziti statickych souboru... JS, CSS... Bez toho nefunguji
+app.UseStaticFiles();
+
+//Propojuje pozadavky na endpointy
+app.UseRouting();
+
+app.UseAuthorization();
+
+//Specifikuje cesty... Tato appka zacina na controlleru Home a akci Index, id volitelne
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
