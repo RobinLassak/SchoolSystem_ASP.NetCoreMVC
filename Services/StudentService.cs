f58fb66e5b14865e79491bfe22069336a2faf25f
Robin <lassak.robin@gmail.com>
2025-05-30 13:54:23 +0200

    Student service

---
+++ Services/StudentService.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NetCoreMVC_SchoolSystem.Services
+{
+    public class StudentService
+    {
+        private readonly SchoolDbContext _dbcontext;
+        public StudentService(SchoolDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+    }
+}
