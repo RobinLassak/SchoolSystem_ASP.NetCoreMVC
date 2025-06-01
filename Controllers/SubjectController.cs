8b47dc0fc2321e1fa04965d68997d1c2daac96fc
Robin <lassak.robin@gmail.com>
2025-06-01 11:33:03 +0200

    Subject Controller+Service+DTO

---
+++ Controllers/SubjectController.cs
@@ -0,0 +1,19 @@
+﻿using ASP.NetCoreMVC_SchoolSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCoreMVC_SchoolSystem.Controllers
+{
+    public class SubjectController : Controller
+    {
+        SubjectService _subjectService;
+        public SubjectController(SubjectService subjectService)
+        {
+            _subjectService = subjectService;
+        }
+        public IActionResult Index()
+        {
+            var allSubjects = _subjectService.GetAll();
+            return View(allSubjects);
+        }
+    }
+}
---
+++ Services/SubjectService.cs
@@ -0,0 +1,33 @@
+﻿using ASP.NetCoreMVC_SchoolSystem.DTO;
+using ASP.NetCoreMVC_SchoolSystem.Models;
+
+namespace ASP.NetCoreMVC_SchoolSystem.Services
+{
+    public class SubjectService
+    {
+        private readonly SchoolDbContext _dbcontext;
+        public SubjectService(SchoolDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        public List<SubjectDTO> GetAll()
+        {
+            var AllSubjects = _dbcontext.Subjects.ToList();
+            var subjectsDtos = new List<SubjectDTO>();
+            foreach (var subject in AllSubjects)
+            {
+                SubjectDTO subjectDTO = ModelToDto(subject);
+                subjectsDtos.Add(subjectDTO);
+            }
+            return subjectsDtos;
+        }
+        private static SubjectDTO ModelToDto(Subject subject)
+        {
+            return new SubjectDTO()
+            {
+                Id = subject.Id,
+                Name = subject.Name,
+            };
+        }
+    }
+}
