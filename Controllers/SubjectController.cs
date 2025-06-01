957c0c9ee20484b54662ac3aa3f1831ed93943b2
Robin <lassak.robin@gmail.com>
2025-06-01 13:55:35 +0200

    Subject view + create new

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -1,4 +1,5 @@
-﻿using ASP.NetCoreMVC_SchoolSystem.Services;
+﻿using ASP.NetCoreMVC_SchoolSystem.DTO;
+using ASP.NetCoreMVC_SchoolSystem.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NetCoreMVC_SchoolSystem.Controllers
@@ -10,10 +11,27 @@ namespace ASP.NetCoreMVC_SchoolSystem.Controllers
         {
             _subjectService = subjectService;
         }
+        //Metoda pro zobrazeni seznamu predmetu
+        [HttpGet]
         public IActionResult Index()
         {
             var allSubjects = _subjectService.GetAll();
             return View(allSubjects);
         }
+        //Metoda pro zobrazeni stranky Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Metoda pro ulozeni noveho predmetu do seznamu
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(SubjectDTO newSubject)
+        {
+            await _subjectService.CreateAsync(newSubject);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- Services/SubjectService.cs
+++ Services/SubjectService.cs
@@ -21,12 +21,28 @@ namespace ASP.NetCoreMVC_SchoolSystem.Services
             }
             return subjectsDtos;
         }
+
+        internal async Task CreateAsync(SubjectDTO newSubject)
+        {
+            Subject subjectToSave = DtoToModel(newSubject);
+            _dbcontext.Subjects.Add(subjectToSave);
+            await _dbcontext.SaveChangesAsync();
+        }
+
         private static SubjectDTO ModelToDto(Subject subject)
         {
             return new SubjectDTO()
             {
                 Id = subject.Id,
                 Name = subject.Name,
+            };
+        }
+        private Subject DtoToModel(SubjectDTO newSubject)
+        {
+            return new Subject()
+            {
+                Id = newSubject.Id,
+                Name = newSubject.Name,  
             };
         }
     }
