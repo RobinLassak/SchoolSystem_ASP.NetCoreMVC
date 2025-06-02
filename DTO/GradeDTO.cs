d0f04d7f2b3faaefaa5ac2943e285a8891e17122
Robin <lassak.robin@gmail.com>
2025-06-02 09:26:29 +0200

    GradeDTO

---
+++ DTO/GradeDTO.cs
@@ -0,0 +1,14 @@
+﻿using ASP.NetCoreMVC_SchoolSystem.Models;
+
+namespace ASP.NetCoreMVC_SchoolSystem.DTO
+{
+    public class GradeDTO
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int SubjectId { get; set; }
+        public string? Topic { get; set; } //Co (zkouseni, pisemka)
+        public int Mark { get; set; } //Znamka
+        public DateTime Date { get; set; }
+    }
+}
