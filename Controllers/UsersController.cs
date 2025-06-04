99d10eab08fbb9fded9ebca58ade2c0c4332bb89
Robin <lassak.robin@gmail.com>
2025-06-04 11:07:33 +0200

    User view + edit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using ASP.NetCoreMVC_SchoolSystem.Models;
+using ASP.NetCoreMVC_SchoolSystem.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCoreMVC_SchoolSystem.Controllers
+{
+    public class UsersController : Controller
+    {
+        UserManager<AppUsers> _userManager;
+        public UsersController(UserManager<AppUsers> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Vytvareni noveho uzivatele
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(UserViewModel newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUsers userToAdd = new AppUsers()
+                {
+                    UserName = newUser.Name,
+                    Email = newUser.Email,
+                };
+                IdentityResult result = await _userManager.CreateAsync(userToAdd, newUser.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(newUser);
+        }
+    }
+}
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NetCoreMVC_SchoolSystem.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
