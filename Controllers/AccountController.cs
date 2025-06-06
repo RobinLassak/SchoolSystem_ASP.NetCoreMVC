af96f77f2a37f85a87816e847d2b9982e790acf2
Robin <lassak.robin@gmail.com>
2025-06-06 20:52:53 +0200

    Login of user solved

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using ASP.NetCoreMVC_SchoolSystem.Models;
+using ASP.NetCoreMVC_SchoolSystem.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCoreMVC_SchoolSystem.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        UserManager<AppUsers> _userManager;
+        SignInManager<AppUsers> _signInManager;
+        public AccountController(UserManager<AppUsers> userManager, SignInManager<AppUsers> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            LoginViewModel model = new LoginViewModel();
+            model.ReturnURL = returnUrl;
+            return View(model);
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        //Prihlaseni uzivatele
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUsers userToLogin = await _userManager.FindByNameAsync(login.UserName);
+                if (userToLogin != null)
+                {
+                    var signInResult = await _signInManager.PasswordSignInAsync(login.UserName, login.Password, false, false);
+                    if (signInResult.Succeeded)
+                    {
+                        return Redirect(login.ReturnURL ?? "/");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "User not found or wrong password");
+            return View(login);
+        }
+        //Odhlaseni uzivatele
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NetCoreMVC_SchoolSystem.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        [Display(Name = "Return URL")]
+        public string? ReturnURL { get; set; }
+    }
+}
