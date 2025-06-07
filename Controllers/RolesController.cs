7a9491df488f3bbcc550f2b398b4f51ba032a731
Robin <lassak.robin@gmail.com>
2025-06-07 17:00:08 +0200

    Roles view

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCoreMVC_SchoolSystem.Controllers
+{
+    public class RolesController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+    }
+}
