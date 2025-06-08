8a391c86c3aaa3096c1978962627213b6fc977ca
Robin <lassak.robin@gmail.com>
2025-06-08 09:51:50 +0200

    Create Role

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -12,8 +12,37 @@ namespace ASP.NetCoreMVC_SchoolSystem.Controllers
         }
         public IActionResult Index()
         {
-            return View(_roleManager.Roles);
+            return View(_roleManager.Roles.OrderBy(role => role.Name));
         }
-
+        //Vytvareni nove role
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddIdentityErrors(result);
+                }
+            }
+            return View(name);
+        }
+        //Pomocne metody
+        private void AddIdentityErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
     }
 }
