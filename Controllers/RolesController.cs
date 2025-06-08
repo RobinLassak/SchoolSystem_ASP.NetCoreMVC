4ad32f9bd411b0be6c79344f2de725e72a174858
Robin <lassak.robin@gmail.com>
2025-06-08 12:18:37 +0200

    Edit role

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using ASP.NetCoreMVC_SchoolSystem.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NetCoreMVC_SchoolSystem.Controllers
@@ -6,9 +7,11 @@ namespace ASP.NetCoreMVC_SchoolSystem.Controllers
     public class RolesController : Controller
     {
         RoleManager<IdentityRole> _roleManager;
-        public RolesController(RoleManager<IdentityRole> roleManager)
+        UserManager<AppUsers> _userManager;
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUsers> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -55,6 +58,25 @@ namespace ASP.NetCoreMVC_SchoolSystem.Controllers
             }
             ModelState.AddModelError("", "Role not found");
             return View("Index", _roleManager.Roles);
+        }
+        //Editace role
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole roleToEdit = await _roleManager.FindByIdAsync(id);
+            List<AppUsers> members = new List<AppUsers>();
+            List<AppUsers> nonMembers = new List<AppUsers>();
+            foreach(var user in _userManager.Users)
+            {
+                var list = await _userManager.IsInRoleAsync(user, roleToEdit.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleState
+            {
+                Members = members,
+                NonMembers = nonMembers,
+                Role = roleToEdit,
+            });
         }
         //Pomocne metody
         private void AddIdentityErrors(IdentityResult result)
---
+++ Models/RoleState.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ASP.NetCoreMVC_SchoolSystem.Models
+{
+    public class RoleState
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<AppUsers> Members { get; set; }
+        public IEnumerable<AppUsers> NonMembers { get; set; }
+    }
+}
