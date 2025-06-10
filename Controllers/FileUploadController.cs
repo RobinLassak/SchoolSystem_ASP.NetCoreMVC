c37582a0d75801e2d2984a99fa8c26e59a7f95b9
Robin <lassak.robin@gmail.com>
2025-06-10 21:36:11 +0200

    upload dataXML

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,42 @@
+﻿using ASP.NetCoreMVC_SchoolSystem.DTO;
+using ASP.NetCoreMVC_SchoolSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ASP.NetCoreMVC_SchoolSystem.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        StudentService _studentService;
+        public FileUploadController(StudentService studentService)
+        {
+            _studentService = studentService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            string filePath = Path.GetFullPath(file.FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+                stream.Close();
+
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(filePath);
+                XmlElement root = xmlDocument.DocumentElement;
+                foreach(XmlNode node in root.SelectNodes("/Students/Student"))
+                {
+                    StudentDTO studentDTO = new StudentDTO()
+                    {
+                        FirstName = node.ChildNodes[0].InnerText,
+                        LastName = node.ChildNodes[1].InnerText,
+                        DateOfBirth = DateOnly.Parse(node.ChildNodes[2].InnerText),
+                    };
+                    await _studentService.CreateAsync(studentDTO);
+                }
+            }
+            return RedirectToAction("Index", "Student");
+        }
+    }
+}
