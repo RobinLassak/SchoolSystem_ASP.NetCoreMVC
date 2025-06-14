2517116a3e5b5379c3154f08a1f4e9c08cdc2315
Robin <lassak.robin@gmail.com>
2025-06-14 16:20:15 +0200

    Teacher DTO

---
+++ DTO/TeacherDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NetCoreMVC_SchoolSystem.DTO
+{
+    public class TeacherDTO
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public int Salary { get; set; }
+    }
+}
