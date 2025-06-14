e147dd7790b3870119416c3ac07aa9e1bfa48ee0
Robin <lassak.robin@gmail.com>
2025-06-14 16:21:17 +0200

    Teacher controller

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,18 @@
+﻿using ASP.NetCoreMVC_SchoolSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCoreMVC_SchoolSystem.Controllers
+{
+    public class TeacherController : Controller
+    {
+        TeacherService _teacherService;
+        public TeacherController(TeacherService teacherService)
+        {
+            _teacherService = teacherService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
